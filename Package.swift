b0e1e3c8b32f7f0ce58b78b34a4dd172de031660
Fazliddinov Iskandar <exclusive.fazliddinov@gmail.com>
2025-05-18 14:17:24 +0500

    replace targets

--- Package.swift
+++ Package.swift
@@ -18,12 +18,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Storage",
-            dependencies: ["Interfaces"]
-        ),
-        .target(
             name: "Interfaces",
             dependencies: []
+        ),
+        .target(
+            name: "Storage",
+            dependencies: ["Interfaces"]
         )
     ]
 )
